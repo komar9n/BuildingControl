e60727b908af0964b4a26049b71dfed1151f6ead
komar9n <komar9n@gmail.com>
2016-06-15 22:45:37 +0200

    Создана сущность User

---
+++ BuildingControl/BuildingControl/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BuildingControl.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserSurName { get; set; }
+        public string UserEmailAddress { get; set; }
+        public string UserPassword { get; set; }
+        public string UserPhoneNumber { get; set; }
+        public DateTime UserDateRegistration { get; set; }
+        public string UserCompany { get; set; }
+        public string UserPosition { get; set; }
+        public int UserGroupId { get; set; }
+        public bool UserActive { get; set; }
+    }
+}
